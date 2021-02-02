4a56323aa0fe3580abbdb7d04242701b49c3bfa1
Julien DUBREUIL <Julien.Dubreuil@bordeaux-inp.fr>
2021-02-02 03:28:11 +0100

    close #4

--- App1/app/src/main/java/com/example/app1/ContactCard.java
+++ App1/app/src/main/java/com/example/app1/ContactCard.java
@@ -5,11 +5,14 @@ package com.example.app1;
  */
 
 import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.OperationApplicationException;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
 import android.view.View;
@@ -21,27 +24,39 @@ import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ContactCard extends AppCompatActivity {
 
-    private static final Uri CONTENT_URI         = ContactsContract.Contacts.CONTENT_URI;
-    private static final Uri PHONE_URI           = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
-    private static final Uri EMAIL_URI           = ContactsContract.CommonDataKinds.Email.CONTENT_URI;
-    private static final Uri ADDR_URI            = ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_URI;
     private static final String ID               = ContactsContract.Contacts._ID;
+    private static final Uri CONTENT_URI         = ContactsContract.Contacts.CONTENT_URI;
     private static final String DISPLAY_NAME     = ContactsContract.Contacts.DISPLAY_NAME;
     private static final String HAS_PHONE_NUMBER = ContactsContract.Contacts.HAS_PHONE_NUMBER;
-    private static final String EMAIL            = ContactsContract.CommonDataKinds.Email.DATA;
-    private static final String STREET           = ContactsContract.CommonDataKinds.StructuredPostal.STREET;
-    private static final String PHONE_ID         = ContactsContract.CommonDataKinds.Phone.CONTACT_ID;
-    private static final String EMAIL_ID         = ContactsContract.CommonDataKinds.Email.CONTACT_ID;
-    private static final String ADDR_ID          = ContactsContract.CommonDataKinds.StructuredPostal.CONTACT_ID;
-    private static final String NUMBER           = ContactsContract.CommonDataKinds.Phone.NUMBER;
     private static final String FAVORITE         = ContactsContract.Contacts.STARRED;
+    private static final String NAME_TYPE        = ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE;
+    private static final String NAME             = ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME;
+
+    private static final String EMAIL_ID         = ContactsContract.CommonDataKinds.Email.CONTACT_ID;
+    private static final Uri EMAIL_URI           = ContactsContract.CommonDataKinds.Email.CONTENT_URI;
+    private static final String EMAIL_ADDRESS    = ContactsContract.CommonDataKinds.Email.ADDRESS;
+    private static final String EMAIL            = ContactsContract.CommonDataKinds.Email.DATA;
+    private static final String EMAIL_TYPE       = ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE;
+
+    private static final String ADDR_ID          = ContactsContract.CommonDataKinds.StructuredPostal.CONTACT_ID;
+    private static final Uri ADDR_URI            = ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_URI;
+    private static final String POSTAL_ADDRESS    = ContactsContract.CommonDataKinds.StructuredPostal.FORMATTED_ADDRESS;
+    private static final String STREET           = ContactsContract.CommonDataKinds.StructuredPostal.STREET;
+    private static final String ADDR_TYPE       = ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_ITEM_TYPE;
+
+    private static final String PHONE_ID         = ContactsContract.CommonDataKinds.Phone.CONTACT_ID;
+    private static final Uri PHONE_URI           = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+    private static final String NUMBER           = ContactsContract.CommonDataKinds.Phone.NUMBER;
+    private static final String PHONE_TYPE       = ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE;
+
+    private static final Uri PROVIDER = android.provider.ContactsContract.Data.CONTENT_URI;
+    private ArrayList<android.content.ContentProviderOperation> ops = new ArrayList<android.content.ContentProviderOperation>();
 
     private String CONTACT_ID;
     private String name;
@@ -61,6 +76,7 @@ public class ContactCard extends AppCompatActivity {
     private TextView displayPhone;
     private TextView displayEmail;
     private TextView displayAddr;
+    private ContentValues contentValues = new ContentValues();
 
     private Toolbar toolbar;
     private CollapsingToolbarLayout toolBarLayout;
@@ -72,6 +88,11 @@ public class ContactCard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_card);
 
+        displayName   = findViewById(R.id.name);
+        displayPhone  = findViewById(R.id.phoneNumber);
+        displayEmail  = findViewById(R.id.mailAddr);
+        displayAddr   = findViewById(R.id.postalAddr);
+
         // recover the contact ID
         intentRecovered = getIntent();
         CONTACT_ID = intentRecovered.getStringExtra("id");
@@ -81,24 +102,35 @@ public class ContactCard extends AppCompatActivity {
         cursor = contentResolver.query(CONTENT_URI,null,ID+ " = ? ",new String[]{CONTACT_ID},null);
         cursor.moveToFirst();
         name = cursor.getString(cursor.getColumnIndex(DISPLAY_NAME));
-        displayName = findViewById(R.id.name);
         displayName.setText(name);
 
         // Recover if it is a favourite contact or not
-        favoriteContact = cursor.getInt(cursor.getColumnIndex(FAVORITE));
         fab = (FloatingActionButton) findViewById(R.id.fab);
+        favoriteContact = cursor.getInt(cursor.getColumnIndex(FAVORITE));
         if (favoriteContact == 1) {
             fab.setForeground(getResources().getDrawable(android.R.drawable.btn_star_big_on));
         }
         else {
             fab.setForeground(getResources().getDrawable(android.R.drawable.btn_star_big_off));
         }
-
+        // Modify if it is a favourite contact or not
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if (favoriteContact == 1) {
+                    favoriteContact = 0;
+                    contentValues.put(FAVORITE, favoriteContact);
+                    contentResolver.update(CONTENT_URI, contentValues, ID + "= ?", new String[]{CONTACT_ID});
+                    fab.setForeground(getResources().getDrawable(android.R.drawable.btn_star_big_off));
+                    contentValues.clear();
+                }
+                else {
+                    favoriteContact = 1;
+                    contentValues.put(FAVORITE, favoriteContact);
+                    contentResolver.update(CONTENT_URI, contentValues, ID + "= ?", new String[]{CONTACT_ID});
+                    fab.setForeground(getResources().getDrawable(android.R.drawable.btn_star_big_on));
+                    contentValues.clear();
+                }
             }
         });
 
@@ -112,7 +144,6 @@ public class ContactCard extends AppCompatActivity {
                 cursor.moveToNext();
             }
             phones = TextUtils.join("\n", phoneList);
-            displayPhone = findViewById(R.id.phoneNumber);
             displayPhone.setText(phones);
             cursor.close();
         }
@@ -121,27 +152,74 @@ public class ContactCard extends AppCompatActivity {
         cursor = contentResolver.query(ADDR_URI,null,ADDR_ID+ " = ? ",new String[]{CONTACT_ID},null);
         if (cursor.moveToNext()) {
             addr = cursor.getString(cursor.getColumnIndex(STREET));
-            displayAddr = findViewById(R.id.postalAddrRoad);
             displayAddr.setText(addr);
         }
 
         // Recover one or many mails
-        cursor = contentResolver.query(EMAIL_URI, null, EMAIL_ID + " = ?", new String[]{CONTACT_ID}, null);
+        cursor = contentResolver.query(EMAIL_URI, new String[]{EMAIL_ADDRESS}, EMAIL_ID + " = ?", new String[]{CONTACT_ID}, null);
         while (cursor.moveToNext()) {
             emailList.add(cursor.getString(cursor.getColumnIndex(EMAIL)));
         }
         if (emailList.size() > 0) {
             emails = TextUtils.join("\n", emailList);
-            displayEmail = findViewById(R.id.mailAddr);
             displayEmail.setText(emails);
             cursor.close();
         }
 
         // launch the toolbar
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        setSupportActionBar(toolbar);
         toolBarLayout.setTitle(getTitle());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        displayName   = findViewById(R.id.name);
+        displayPhone  = findViewById(R.id.phoneNumber);
+        displayEmail  = findViewById(R.id.mailAddr);
+        displayAddr   = findViewById(R.id.postalAddr);
+
+        name    = displayName.getText().toString();
+        emails  = displayEmail.getText().toString();
+        phones  = displayPhone.getText().toString();
+        addr    = displayAddr.getText().toString();
+
+        String where = ContactsContract.Data.CONTACT_ID + " = ? AND " + ContactsContract.Data.MIMETYPE + " = ?";
+
+        if(!name.equals("")) {
+            ops.add(android.content.ContentProviderOperation.newUpdate(PROVIDER)
+                    .withSelection(where,new String[]{CONTACT_ID, NAME_TYPE})
+                    .withValue(NAME, name)
+                    .build());
+        }
+        if (!emails.equals(R.id.mailAddr)) {
+                ops.add(android.content.ContentProviderOperation.newUpdate(PROVIDER)
+                        .withSelection(where,new String[]{CONTACT_ID, EMAIL_TYPE})
+                        .withValue(EMAIL, emails)
+                        .build());
+        }
+        if(!phones.equals(R.id.phoneNumber)) {
+            ops.add(android.content.ContentProviderOperation.newUpdate(PROVIDER)
+                    .withSelection(where,new String[]{CONTACT_ID, PHONE_TYPE})
+                    .withValue(NUMBER, phones)
+                    .build());
+        }
+        if(!addr.equals(R.id.postalAddr)) {
+            ops.add(android.content.ContentProviderOperation.newUpdate(PROVIDER)
+                    .withSelection(where,new String[]{CONTACT_ID, ADDR_TYPE})
+                    .withValue(POSTAL_ADDRESS, addr)
+                    .build());
+        }
+
+        try {
+            contentResolver.applyBatch(ContactsContract.AUTHORITY, ops);
+        } catch (OperationApplicationException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 
     // This method permits to open Google Maps to have the route to the contact address
